dd69ce393030782660bebe20e04c66815ed9dbaf
circuitbreakr <WHOTOOKCIRCUIT@gmail.com>
2025-04-13 02:43:28 -0400

    styling

--- frontend/src/components/Graph.js
+++ frontend/src/components/Graph.js
@@ -2,8 +2,8 @@ import Container from 'react-bootstrap/Container';
 
 export default function Graphs({ taxonId }) {
   return (
-    <Container className="py-4">
-      <h2 className="mb-3">Population Forecast</h2>
+    <Container fluid className="ms-4 py-4">
+      <h4 className="ms-2 mb-3">Population Forecast</h4>
       <iframe
         title={`Population Graph for Taxon ${taxonId}`}
         src={`http://localhost:5000/graph/${taxonId}`}
