62cfc5559f0e43a29fac5e2178706036e6059cb9
shinycosmic <razorrotom@gmail.com>
2025-04-12 21:45:12 -0400

    Connecting Functions

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,13 @@
 from flask import Flask,jsonify
 import requests
 import json
+import pandas as pd
 
 app = Flask(__name__)
+df = pd.read_csv('filtered_species_with_conservation_status.csv')
+taxon_dict = dict(zip(df['taxon_id'], df['name']))
+
+
 
 
 #TODO: direct to search page
@@ -11,11 +16,21 @@ def hello():
     return 'Hello, World!'
 
 
+
 #TODO: main function to run the code to get the information we need. We can use auxillary functions, but this function returns all the information the animal page will use including the data.
 @app.route('/animal/<animal_name>')
-def animal_search(animal_name):
 
-    pass
+def animal_search(animal_name):
+    suggest_species(animal_name)
+
+def suggest_species(query, max_results=5):
+    query = query.lower()
+    matches = [
+        {"name": name, "taxon_id": taxon_id}
+        for name, taxon_id in taxon_dict.items()
+        if query in name.lower()
+    ]
+    return matches[:max_results]
 
 @app.route('/image/<taxon_id>')
 def get_img(taxon_id):
@@ -38,4 +53,31 @@ def get_img(taxon_id):
 
 @app.route('/info/<taxon_id>')
 def info(taxon_id):
+   #preferred_common_name (name), iconic_taxon_name (genus), name (species), wikipedia_url
+    url = f'https://api.inaturalist.org/v1/taxa/{taxon_id}?order=desc&order_by=observations_count'
+    response = requests.get(url)
+    ob = response.json()
+
+   # Handle unexpected responses
+    if response.status_code != 200 or 'results' not in ob:
+        return jsonify({'error': 'API request failed or malformed response'}), 500
+
+    if ob.get("total_results", 0) == 0:
+        return jsonify({'error': 'No results found'}), 404
     
+    try:
+        name = ob['results'][0]['preferred_common_name']
+        genus = ob['results'][0]['iconic_taxon_name']
+        species = ob['results'][0]['name']
+        wikipedia_url = ob['results'][0]['wikipedia_url']
+        return jsonify({'name': name, 
+                        'genus': genus,
+                        'species': species,
+                        'wikipedia_url': wikipedia_url})
+    except (KeyError, IndexError):
+        return jsonify({'error': 'Info not available for this taxon'}), 404
+    
+
+@app.route('/map/<taxon_id>')
+def map_dist(taxon_id):
+    pass
