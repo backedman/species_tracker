0e6a135d2fd6f15b6265a0c535b76b2d3ff0abec
backedman <backedman@gmail.com>
2025-04-12 20:04:22 -0400

    animal picture added

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask,jsonify
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -12,4 +14,24 @@ def hello():
 #TODO: main function to run the code to get the information we need. We can use auxillary functions, but this function returns all the information the animal page will use including the data.
 @app.route('/animal/<animal_name>')
 def animal_search(animal_name):
-    
+
+    pass
+
+@app.route('/image/<taxon_id>')
+def get_img(taxon_id):
+    url = f'https://api.inaturalist.org/v1/taxa/{taxon_id}?order=desc&order_by=observations_count'
+    response = requests.get(url)
+    ob = response.json()
+
+    # Handle unexpected responses
+    if response.status_code != 200 or 'results' not in ob:
+        return jsonify({'error': 'API request failed or malformed response'}), 500
+
+    if ob.get("total_results", 0) == 0:
+        return jsonify({'error': 'No results found'}), 404
+
+    try:
+        image_url = ob['results'][0]['default_photo']['url']
+        return jsonify({'image_url': image_url})
+    except (KeyError, IndexError):
+        return jsonify({'error': 'Image not available for this taxon'}), 404
