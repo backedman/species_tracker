62ebc52e754f80327fc1286877e97f705fcc6944
circuitbreakr <WHOTOOKCIRCUIT@gmail.com>
2025-04-12 23:29:03 -0400

    info display for searches

--- frontend/src/components/BasicInfo.js
+++ frontend/src/components/BasicInfo.js
@@ -4,25 +4,41 @@ import Image from 'react-bootstrap/Image';
 
 export default function BasicInfo({ taxonId }) {
   const [imageUrl, setImageUrl] = useState('');
+  const [info, setInfo] = useState('');
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    
-    
     if (!taxonId) return;
 
     fetch(`/image/${taxonId}`)
       .then((res) => res.json())
       .then((data) => {
-        if (data.image_url) setImageUrl(data.image_url);
-        else setError('No image found.');
+        if (data.image_url){
+          setImageUrl(data.image_url);
+          console.log(imageUrl);
+        } else {
+          setError('No image found.');
+        }
       })
       .catch((err) => {
         console.error(err);
         setError('Failed to load image.');
       });
-
-      fetch(`/info/${taxonId}`)
+    
+    fetch(`/info/${taxonId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.genus){
+          setInfo(data);
+          console.log(info);
+        } else {
+          setError('No info found.');
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        setError('Failed to load info.');
+      });
   }, [taxonId]);
 
   return (
@@ -38,9 +54,11 @@ export default function BasicInfo({ taxonId }) {
         />
       )}
       <Container>
-        <h3 className = "ms-4">Taxa Name</h3>
+        <h3 className = "ms-4">{info.name}</h3>
         <p id = "description" className = "ms-4">
-            INSERT PROFILE TEXT HERE
+          <strong>Class:</strong> {info.genus}<br/>
+          <strong>Species:</strong> <em>{info.species}</em><br/>
+          <strong>Wikipedia Link:</strong> {info.wikipedia_link}<br/>
         </p>
       </Container>
     </Container>
